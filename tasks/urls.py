fd9c3e97971cd158da8bfd1ebcb7d18cb42fca44
kira <contacto@contaco.com>
2020-04-21 17:21:49 -0500

    hasta actualizar redirect no style

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="list"),
+    path('update/<str:pk>/',views.updateTask,name="update_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -16,3 +16,14 @@ def index(request):
         return redirect('/')
     context = {'info':consulta,'temp':form}
     return render(request,"tasks/list.html", context)
+
+def updateTask(request, pk):
+    item = Task.objects.get(id=pk)
+    form_update = TaskForm(instance=item)
+    if request.method == 'POST':
+        form_update = TaskForm(request.POST, instance = item)
+        if form_update.is_valid():
+            form_update.save()
+            return redirect('/')
+    context = {'form_update':form_update}
+    return render(request, 'tasks/update_task.html',context)
