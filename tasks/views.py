4f00df9ff0b026d8512f811321b7db0f303ed32c
kira <contacto@contaco.com>
2020-04-21 16:10:53 -0500

    agrega las tareas y redirecciona a la misma pag

--- tasks/views.py
+++ tasks/views.py
@@ -3,10 +3,16 @@ from django.http import HttpResponse
 
 from tasks.models import Task
 from tasks.forms import TaskForm
+from django.shortcuts import redirect
 # Create your views here.
 
 def index(request):
     consulta = Task.objects.all()
     form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
     context = {'info':consulta,'temp':form}
     return render(request,"tasks/list.html", context)
